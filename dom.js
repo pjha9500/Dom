10b0294ff0a45165655de8b4838d1f7a42a910f8
pjha9500 <64766200+pjha9500@users.noreply.github.com>
2022-06-18 12:52:49 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,2 +1,11 @@
 document.getElementById('Additem').style.fontWeight='bold';
 document.getElementById('Additem').style.color='green';
+
+//Make the 3 rd element in the list have green background color
+//Make all the elements in the list have bold color font
+var items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor="green";
+for(let i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight='bold';
+}
